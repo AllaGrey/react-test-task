9c1370dfa53d607a4b33d3330bd66b850338e4d6
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-05-21 14:31:10 +0300

    completed task

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Header, StyledNavLink, Footer, FooterDetails } from './Layout.styled';
+export const Layout = () => {
+  return (
+    <div>
+      <Header>
+        <StyledNavLink to="/">Home</StyledNavLink>
+        <StyledNavLink to="/tweets">Tweets</StyledNavLink>
+      </Header>
+      <Suspense fallback={null}>
+        <Outlet />
+      
+        <Footer>
+          <FooterDetails href='https://github.com/AllaGrey' noopener noreferrer nofollow >Created by Alla Grey </FooterDetails>
+        </Footer>
+      </Suspense>
+    </div>
+  );
+};
---
+++ src/components/Styles/Container.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding-left: 15px;
+    padding-right: 15px;
+    margin: 0 auto;
+    background-color: #e3d0f2;
+`
